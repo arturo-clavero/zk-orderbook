675d0ff8aeaf32f32c75f71c7d03f7f283d086c1
Arturo Clavero <arturoclavero01@gmail.com>
2025-10-19 19:37:01 +0700

    Defeat: circuit explodes at 9 action verification, single batch circuit for account model tree updates does not work

--- proofs/verify.js
+++ proofs/verify.js
@@ -5,25 +5,22 @@ import { UltraHonkBackend } from '@aztec/bb.js';
 import { Noir } from '@noir-lang/noir_js';
 import { verifyDeposits, verifyFirstDeposits } from './actions/deposit.js';
 import os from 'node:os';
+import { verifyWithdrawals } from './actions/withdraw.js';
 
 const PREPATH = '../circuits/';
 const MAX_ACTIONS = 3;
+
 export async function verifyBatch(){
   const fd = await verifyFirstDeposits(MAX_ACTIONS);
   const d = await verifyDeposits(MAX_ACTIONS);
+  const w = await verifyWithdrawals(MAX_ACTIONS);
 
-  //if there is nothing to verify return...
   if (fd.newRoot == '-1' && d.rootAfter == '-1')
     return;
-  
+
   const oldRoot = fd.newRoot == '-1' ? d.oldRoot : fd.oldRoot;
   const newRoot = d.newRoot == '-1' ? fd.newRoot : d.newRoot;
-  console.log("d: ", fd);
-  // console.log("first deposits: ", fd);
-  // console.log("deposits: ", d);
-  // console.log("big old root: ", oldRoot);
-  // console.log("big new root: ", newRoot);
-  
+
   const inputs = {
     oldRoot: oldRoot.toString(),
     newRoot: newRoot.toString(),
@@ -31,9 +28,11 @@ export async function verifyBatch(){
     totalFirstDeposits: fd.totalFirstDeposits,
     deposits: d.deposits,
     totalDeposits: d.totalDeposits,
-    //withdraw: w.withdraws,
-    //totalWithdraw: w.totalWithdraws;
+    withdrawals: w.withdrawals,
+    withdrawalDetails: w.withdrawalDetails,
+    totalWithdrawals: w.totalWithdrawals,
   }
+
   console.log("final inputs: ", inputs);
   return await callCircuit(inputs, 'batch');
 }
@@ -56,3 +55,36 @@ export async function callCircuit(inputs, path){
     return false;
   }
 }
+
+export async function getRecursive(inputs, path) {
+  try {
+    const compiledCircuit = await compile(
+      createFileManager(
+        resolve(dirname(fileURLToPath(import.meta.url)), `${PREPATH}${path}`)
+      )
+    );
+
+    const noir = new Noir(compiledCircuit.program);
+    const backend = new UltraHonkBackend(compiledCircuit.program.bytecode, {
+      threads: os.cpus().length,
+    });
+
+    const { witness } = await noir.execute(inputs);
+    const { proof, publicInputs } = await backend.generateProof(witness);
+    const verificationKey = await backend.getVerificationKey();
+
+  console.log("public inputs: ", publicInputs);
+    return {
+      recursive : {
+        proof,
+        verificationKey,
+      },
+      publicInputs,
+    };
+
+  } catch (error) {
+    console.error("Error in recursive:", error);
+    throw error;
+  }
+}
+
