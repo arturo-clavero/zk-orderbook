7973b7756e2a004a55df571c818a0c1b8052ee0e
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-10-19 18:36:12 +0700

    fix: pyusdt counts// usdt incorect format//eth not creating account

--- backend/src/deposit/deposit-controller.ts
+++ backend/src/deposit/deposit-controller.ts
@@ -1,44 +1,71 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Controller, Post, Body } from '@nestjs/common';
 import { prisma } from '../lib/prisma-trading-database/prisma-trading';
 
+const PYUSD_ADDRESS = process.env.PYUSD_ADDRESS?.toLowerCase() ?? '';
+const USDT_ADDRESS = process.env.USDT_ADDRESS?.toLowerCase() ?? '';
+console.log('address id ', PYUSD_ADDRESS);
+console.log('address id ', USDT_ADDRESS);
+export const TOKEN_MAP: Record<string, string> = {
+  [PYUSD_ADDRESS]: 'PYUSD',
+  [USDT_ADDRESS]: 'USDT',
+};
+
 @Controller('deposit')
 export class DepositController {
   @Post()
   async handleDeposit(@Body() dataEnvio: any) {
-    const { traderId, token, amount, txHash } = dataEnvio;
-    console.log('data which backend recevies is', dataEnvio);
+    let { traderId, token, amount, txHash } = dataEnvio;
+    traderId = traderId.toLowerCase();
+    token = token.toLowerCase();
+
+    console.log('data which backend recevies is in the deposit', dataEnvio);
     if (!traderId || !token || !amount || !txHash) {
       return {
         success: false,
         message: 'Missing data',
       };
     }
-    //find/create trader
+    //find trader
     const trader = await prisma.trader.upsert({
       where: { address: traderId },
       create: { address: traderId },
       update: {},
     });
-    // Creat/check if acount exists
+    console.log('Trader which was not found is ', trader);
+    const currency = TOKEN_MAP[token] || token;
+    // check if acount exists
     const account = await prisma.account.findFirst({
       where: {
         traderId: trader.id,
-        currency: token,
+        currency: currency,
       },
     });
     if (!account) {
+      console.log('Account which was not found is ', account);
       console.error('Account was not found, action will be ignnored');
       return;
     }
     try {
+      const existing = await prisma.transaction.findUnique({
+        where: { txHash },
+      });
+      if (existing) {
+        console.log(`Hash ${txHash} for this transaction exists, skipping`);
+        return {
+          success: false,
+          message: 'Transaction is already created',
+        };
+      }
       //kyc if it will be time
       console.log('received from frontend', dataEnvio);
       // to db
-      const deposit = await prisma.transaction.create({
-        data: {
+      const deposit = await prisma.transaction.upsert({
+        where: { txHash },
+        create: {
           type: 'deposit',
           traderId: trader.id,
           token,
@@ -46,6 +73,7 @@ export class DepositController {
           status: txHash ? 'Confirmed' : 'Pending',
           txHash: txHash || null,
         },
+        update: {},
       });
       if (deposit.status === 'Confirmed') {
         await prisma.account.update({
@@ -54,10 +82,6 @@ export class DepositController {
         });
         console.log('Saved to dataabase wtih id', deposit.id);
       }
-      const existing = await prisma.transaction.findUnique({
-        where: { txHash },
-      });
-      if (existing) return;
       //what backend decided
       return {
         success: true,
