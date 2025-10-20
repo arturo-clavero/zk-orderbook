2578d4947913272505b4404feb1a693241377457
Arturo Clavero <arturoclavero01@gmail.com>
2025-10-20 13:43:40 +0700

    feat: merkle tree circuits

---
+++ proofs/circuit.js
@@ -0,0 +1,27 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'node:path';
+import { compile, createFileManager } from '@noir-lang/noir_wasm';
+import { UltraHonkBackend } from '@aztec/bb.js';
+import { Noir } from '@noir-lang/noir_js';
+import os from 'node:os';
+
+const PREPATH = '../circuits/';
+
+export async function callCircuit(inputs, path){
+  try {
+    const compiledCircuit = await compile(createFileManager(
+      resolve(dirname(fileURLToPath(import.meta.url)), `${PREPATH}${path}`)
+    ));
+
+    const noir = new Noir(compiledCircuit.program);
+    const backend = new UltraHonkBackend(compiledCircuit.program.bytecode, { threads: os.cpus().length });
+    // const backend = new UltraHonkBackend(compiledCircuit.program.bytecode, { threads: 1});
+    const { witness } = await noir.execute(inputs, { showOutputs: true });
+    const { proof, publicInputs } = await backend.generateProof(witness);
+    console.log("verifying...");
+    return await backend.verifyProof({ proof, publicInputs });
+  } catch(error){
+    console.error("Verification failed:", error);
+    return false;
+  }
+}
