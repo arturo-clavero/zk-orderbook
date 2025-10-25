734c3e520366393d84b93da9868300e6119090fb
Valeria <70722334+Hikabu@users.noreply.github.com>
2025-10-25 08:04:17 +0700

    no redis

--- envio/src/EventHandlers.ts
+++ envio/src/EventHandlers.ts
@@ -5,81 +5,53 @@ import {
   Deposit,
   Deposit_Deposit,
 } from "generated";
-
 import axios from 'axios';
-import Redis from "ioredis";
+import { error } from "console";
+import { execPath } from "process";
 import  { ethers } from "ethers";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST || 'redis',
-  port: Number(process.env.REDIS_PORT || '6379'),
-  
-})
-
 //corrupted "amount" value
-function normalizer(token: string, amount: string | number | bigint): string {
-  const decimals = token.toUpperCase() === "ETH" ? 18 : 6;
+function normilizer(token: string, amount: string | number | bigint): string {
+  let decimals: number;
+  switch (token.toUpperCase()){
+    case "ETH":
+      decimals = 18;
+      break;
+    case "PYUSD":
+    case "ETH":
+    default:
+      decimals = 6;
+      break;
+  }
   return ethers.formatUnits(amount.toString(), decimals);
 }
 
 Deposit.Deposit.handler(async ({ event, context }) => {
-  const user = event.params.user;
-  const token =  event.params.token.toUpperCase();
-  const amount =  event.params.amount;
-  const txHash = event.transaction.hash;
-
-  const humanAmount = normalizer(token, amount);
-      //redis stuff store deposit record
-  const redisKey = `deposit:${txHash}`;
-
-  await redis.hset(redisKey, {
-    user,
-    token,
-    rawAmount: amount.toString(),
-    humanAmount: humanAmount,
-    txHash: txHash,
-    timestamp: Date.now.toString(),
-  });
-
-  //update user balance
-  const balanceKey = `balance:${user}`
-  const prev = await redis.hget(balanceKey, token);
-  const updated = (parseFloat(prev || "0") + parseFloat(humanAmount)).toString();
-  await redis.hset(balanceKey, token, updated);
-
-  //Publish evnts
-  await redis.publish(
-    "deposits",
-    JSON.stringify({
-      user, token, rawAmount: amount.toString(), txHash })
-  );
-  //index for envio
-  context.Deposit_Deposit.set({
+  const entity: Deposit_Deposit = {
     id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
-    user,
-    token,
-    amount,
-    txHash,
-  });
+    user: event.params.user,
+    token: event.params.token,
+    amount: event.params.amount,
+    txHash: event.transaction.hash,
+  };
+
+  const normilizedAmount = normilizer(entity.token, entity.amount);
+  context.Deposit_Deposit.set(entity);
+  try {
+    const response = await axios.post('http://localhost:4000/deposit', 
+      {
+        traderId: entity.user,
+        token: entity.token,
+        rawAmount: entity.amount.toString(),
+        txHash: entity.txHash,
+      }
+    );
+    console.error("DEBUG", entity);
+  }catch(err){
+    if ( err instanceof Error){
+      console.log(err.message);
+    } else {
+      console.log('Unexpected error', err);
+    }
+  }
 });
-
-////axios save to postgres
-  // context.Deposit_Deposit.set(entity);
-  // try {
-  //   const response = await axios.post('http://localhost:4000/deposit', 
-  //     {
-  //       traderId: entity.user,
-  //       token: entity.token,
-  //       rawAmount: entity.amount.toString(),
-  //       txHash: entity.txHash,
-  //     }
-  //   );
-  //   console.error("DEBUG", entity);
-  // }catch(err){
-  //   if ( err instanceof Error){
-  //     console.log(err.message);
-  //   } else {
-  //     console.log('Unexpected error', err);
-  //   }
-  // }
-
