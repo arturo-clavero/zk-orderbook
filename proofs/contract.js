42e8aaedff9e174ed3c4902c4c2a263b784bbc7d
Arturo Clavero <arturoclavero01@gmail.com>
2025-10-26 15:37:06 +0700

    fix: verifier is working

--- proofs/contract.js
+++ proofs/contract.js
@@ -12,15 +12,21 @@ export async function callSmartContract(id, proof, publicInputs, nullifiers, wit
   const PRIVATE_KEY = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 
-  const DARKPOOL_ADDRESS = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+  const DARKPOOL_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 
   const provider = new JsonRpcProvider(RPC_URL);
   const wallet = new Wallet(PRIVATE_KEY, provider);
   const darkPool = new Contract(DARKPOOL_ADDRESS, DarkPool.abi, wallet);
   
-    const selector5 = ethers.id("ProofLengthWrong()").slice(0,10);
+  const selector5 = ethers.id("ProofLengthWrong()").slice(0,10);
   console.log("5: ", selector5); 
+  const selector4 = ethers.id("PublicInputsLengthWrong()").slice(0,10);
+  console.log("4: ", selector4); 
+    const selector3 = ethers.id("SumcheckFailed()").slice(0,10);
+  console.log("3: ", selector3); 
+    const selector2 = ethers.id("ShpleminiFailed()").slice(0,10);
+  console.log("2: ", selector2); 
 
   const rootBefore = await darkPool.root();
   
